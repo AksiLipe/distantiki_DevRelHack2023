16a0cb7972fc4e1b30a0426d29891cd32c6b0234
Ilmir Sharifullin <ilmirsharifullin2000@gmail.com>
2023-12-15 21:35:04 +0300

    pymongo activate

--- devrelhack/settings.py
+++ devrelhack/settings.py
@@ -74,16 +74,10 @@ WSGI_APPLICATION = 'devrelhack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'devrelhack',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["devrelhack"]
 
 
 # Password validation
