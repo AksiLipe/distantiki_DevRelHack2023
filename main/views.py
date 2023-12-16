f68dcc602246d2c40052326b4dc945f357df4ead
Ilmir Sharifullin <ilmirsharifullin2000@gmail.com>
2023-12-16 18:54:39 +0300

    table back ready

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
 from devrelhack.settings import db
 from main.create_members import create_member
@@ -8,41 +8,42 @@ def index(request):
     return render(request, "base.html")
 
 
-def table_view(request):
+def table_view(request: HttpRequest):
     attributes = db.get_collection("attributes").find_one().get("attributes")
-    cursor = db.get_collection("members").find()
-    members = list(cursor)
-    print(attributes)
-    print(members)
-    data = [
-        {"_id": "657cb67287d6f3db2363a86c", "Почта": "ilmirsharifullin2000@gmail.com", "Имя": "Ilmir",
-         "Фамилия": "Sharifullin", "Номер Телефона": 79520367755, "Телеграм": "dedailmir"},
-        {"_id": "657cb82180271735dfeff498", "Почта": "qwerty@example.com", "Имя": "Вадим", "Фамилия": "Филиппов",
-         "Номер Телефона": 7987654321},
-        {"_id": "657d5db27ea97f66a6694068", "Имя": "Дима", "Почта": "asdfgh@gmail.com", "Телеграм": "dedadima"},
-        {"_id": "657d5e310ce084be218c9d67", "Почта": "zxc@example.com", "Имя": "Иван", "Фамилия": "Иванов",
-         "Номер Телефона": 79876543210, "Телеграм": "qwerty123"}
-    ]
+    db_members = db.get_collection('members')
+    members = list(db_members.find())
+    for attribute, meta in attributes.items():
+        if meta.get('filter', 0) == 1:
+            values = {member.get(attribute, '') for member in members}
+            meta['select'] = values
+            attributes[attribute] = meta
 
-    headers = {
-        "Почта": {"unique": 1, "filter": 0},
-        "Номер Телефона": {"unique": 1, "filter": 0},
-        "Имя": {"unique": 0, "filter": 0},
-        "Фамилия": {"unique": 0, "filter": 0},
-        "Телеграм": {"unique": 1, "filter": 0},
-        "Стаж": {"unique": 0, "filter": 1},
-        "Специализация": {"unique": 0, "filter": 1}
-    }
-    headers = headers.keys()
-    return render(request, "table.html", {"data": data, "headers": headers})
+    is_ajax = request.headers.get('x-requested-with') == 'XMLHttpRequest'
+    if is_ajax:
+        filters = request.GET.dict()
+        for attribute, values in filters.items():
+            if values:
+                values = values[1:].split(';')
+                filtered_members = []
+                for m in members:
+                    if m.get(attribute, '') in values:
+                        filtered_members.append(m)
+                members = filtered_members
+        table_html = ''
+        for item in members:
+            table_html += '<tr>\n'
+            for header in attributes:
+                table_html += f'<td>{item.get(header, "")}</td>\n'
+            table_html += '</tr>'
+        return JsonResponse({'table_html': table_html})
+
+    return render(request, "table.html", {"data": members, "headers": attributes})
 
 
 def create_member_view(request):
     attributes = db.get_collection("attributes").find_one().get("attributes")
     cursor = db.get_collection("members").find()
     members = list(cursor)
-    print(attributes)
-    print(members)
     if request.method == "POST":
         data = {}
         for field_name in attributes:
@@ -52,6 +53,11 @@ def create_member_view(request):
         if res:
             return HttpResponse("Данные успешно добавлены!")
         else:
-            return render(request, "add_member.html", {"fields": attributes, 'error': 'Используются уникальные поля разных пользователей'})
+            return render(request, "add_member.html",
+                          {"fields": attributes, 'error': 'Используются уникальные поля разных пользователей'})
     else:
         return render(request, "add_member.html", {"fields": attributes})
+
+
+def import_table_view(request):
+    pass
