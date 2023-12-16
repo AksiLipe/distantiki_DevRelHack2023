d7ccb41f8e63f80a36cf687b133c5be847df4d7b
Ilmir Sharifullin <ilmirsharifullin2000@gmail.com>
2023-12-17 02:15:54 +0300

    analytics and etc

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import table_view, create_member_view, import_members, index
+from .views import table_view, create_member_view, import_members, index, get_analytics
 
 urlpatterns = [
     path('',  index),
     path('table/', table_view, name='table-view'),
     path('add_member/', create_member_view, name='add-member'),
-    path('import_member/', import_members, name='import-member'),
+    path('import_members/', import_members, name='import-members'),
+    path('analytics', get_analytics, name='analytics')
 ]
