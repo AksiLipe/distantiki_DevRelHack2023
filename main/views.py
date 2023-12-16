0cff72d099bbd5d5975c30f3f3749ad8c966de5e
AksiLipe <137812264+AksiLipe@users.noreply.github.com>
2023-12-17 01:31:58 +0300

    update base, import_members, style, urls, views, add_member, malling, table

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-
 from bson import ObjectId
 from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.shortcuts import render, redirect
@@ -11,9 +10,23 @@ def index(request):
 
 
 def table_view(request: HttpRequest):
-    attributes = db.get_collection("attributes").find_one().get("attributes")
-    db_members = db.get_collection('members')
-    members = list(db_members.find())
+    attributes = {'Почта': {'unique': 1, 'filter': 0}, 'Номер Телефона': {'unique': 1, 'filter': 0},
+                  'Имя': {'unique': 0, 'filter': 0}, 'Фамилия': {'unique': 0, 'filter': 0},
+                  'Телеграм': {'unique': 1, 'filter': 0},
+                  'Стаж': {'unique': 0, 'filter': 1},
+                  'Специализация': {'unique': 0, 'filter': 1}}
+    members = [{'_id': ObjectId('657cb67287d6f3db2363a86c'), 'Почта': 'ilmirsharifullin2000@gmail.com', 'Имя': 'Ilmir',
+                'Фамилия': 'Sharifullin', 'Номер Телефона': 79520367755, 'Телеграм': 'dedailmir',
+                'Специализация': 'backend',
+                'Стаж': '1-3 года'},
+               {'_id': ObjectId('657cb82180271735dfeff498'), 'Почта': 'qwerty@example.com', 'Имя': 'Вадим',
+                'Фамилия': 'Филиппов',
+                'Номер Телефона': 7987654321, 'Специализация': 'backend', 'Стаж': '1-3 года'},
+               {'_id': ObjectId('657d5db27ea97f66a6694068'), 'Имя': 'Дима', 'Почта': 'asdfgh@gmail.com',
+                'Телеграм': 'dedadima'},
+               {'_id': ObjectId('657d5e310ce084be218c9d67'), 'Почта': 'zxc@example.com', 'Имя': 'Иван',
+                'Фамилия': 'Иванов',
+                'Номер Телефона': 79876543210, 'Телеграм': 'qwerty123', 'Специализация': 'frontend', 'Стаж': '<1 года'}]
     for attribute, meta in attributes.items():
         if meta.get('filter', 0) == 1:
             values = {member.get(attribute, '') for member in members}
@@ -42,7 +55,11 @@ def table_view(request: HttpRequest):
 
 
 def create_member_view(request):
-    attributes = db.get_collection("attributes").find_one().get("attributes")
+    attributes = {'Почта': {'unique': 1, 'filter': 0}, 'Номер Телефона': {'unique': 1, 'filter': 0},
+                  'Имя': {'unique': 0, 'filter': 0}, 'Фамилия': {'unique': 0, 'filter': 0},
+                  'Телеграм': {'unique': 1, 'filter': 0},
+                  'Стаж': {'unique': 0, 'filter': 1},
+                  'Специализация': {'unique': 0, 'filter': 1}}
     if request.method == "POST":
         data = {}
         for field_name in attributes:
@@ -57,5 +74,9 @@ def create_member_view(request):
     else:
         return render(request, "add_member.html", context={"fields": attributes})
 
+
 def import_members(request):
     return render(request, "import_members.html")
+
+def mailing(request):
+    return render(request, "mailing.html")
