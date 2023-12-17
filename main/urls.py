c3cedec9df9da269b1af2e041400f0ad09bfbf57
Ilmir Sharifullin <ilmirsharifullin2000@gmail.com>
2023-12-17 10:02:28 +0300

    fix

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('table/', table_view, name='table-view'),
     path('add_member/', create_member_view, name='add-member'),
     path('import_members/', import_members, name='import-members'),
-    path('analytics', get_analytics, name='analytics'),
+    path('analytics/', get_analytics, name='analytics'),
     path('malling/', mailing, name='mailing'),
 ]
